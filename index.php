75337927d6dc2fc83c8f04b2aa55542e9683375a
unknown <catalinapradilla@hotmail.com>
2017-10-27 14:06:13 +0200

    Added scripts again

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require 'database.php';
 
-
 require 'todo.php';
 
 
@@ -96,6 +95,8 @@ require 'todo.php';
 </div> <!--wrapper-->
 </body>
 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 </html>
